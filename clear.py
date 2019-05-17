f77c17303d516d62484d7218ac5c996da1c4b66a
alingse <alingse@foxmail.com>
2019-05-17 23:20:37 +0800

    添加

--- clear.py
+++ clear.py
@@ -1,3 +1,6 @@
+# coding=utf8
+
+import re
 import sys
 import json
 
@@ -5,23 +8,70 @@ from json.decoder import JSONDecoder
 from json.scanner import py_make_scanner
 
 
+"""
+def errmsg(msg, doc, pos, end=None):
+    # Note that this function is called from _json
+    lineno, colno = linecol(doc, pos)
+    if end is None:
+        fmt = '{0}: line {1} column {2} (char {3})'
+        return fmt.format(msg, lineno, colno, pos)
+        #fmt = '%s: line %d column %d (char %d)'
+        #return fmt % (msg, lineno, colno, pos)
+    endlineno, endcolno = linecol(doc, end)
+    fmt = '{0}: line {1} column {2} - line {3} column {4} (char {5} - {6})'
+    return fmt.format(msg, lineno, colno, endlineno, endcolno, pos, end)
+    #fmt = '%s: line %d column %d - line %d column %d (char %d - %d)'
+    #return fmt % (msg, lineno, colno, endlineno, endcolno, pos, end)
+"""
+
+# errmsg.inv
+def inv_errmsg(e):
+    message = e.message
+
+    err, left = message.split(':', 1)
+    numbers = re.compile(r'\d+').findall(left)
+    result = {
+        "err": err,
+        "lineno": int(numbers[0]),
+        "colno": int(numbers[1]),
+    }
+    if len(numbers) == 3:
+        result["pos"] = int(numbers[2])
+
+    if len(numbers) > 3:
+        result["endlineno"] = int(numbers[2])
+        result["endcolno"] = int(numbers[3])
+        result["pos"] = int(numbers[4])
+        result["end"] = int(numbers[5])
+    return result
+
+
+# 暂时只考虑 1 行的情况
 def find_stop(line):
     d = JSONDecoder()
     d.scan_once = py_make_scanner(d)
-    # TODO
     try:
+        # import pdb
+        # pdb.set_trace()
         obj, end = d.scan_once(line, 0)
-    except StopIteration as e:
+        return line
+    except ValueError as e:
+        err_info = inv_errmsg(e)
+        pos = err_info["pos"]
+        if err_info["err"] == "Expecting object":
+            return insert_line(line, "null", pos)
+        if err_info["err"] == "Expecting ',' delimiter":
+            if line[pos:] == "":
+                return insert_line(line, "}", pos)
+            return insert_line(line, ",", pos)
         raise e
-    return line
+
+
+def insert_line(line, value, pos, end=None):
+    return line[:pos] + value + line[pos:]
 
 
 def clear(line):
-    try:
-        json.load(line)
-        return line
-    except Exception as e:
-        pass
     new_line = find_stop(line)
     return new_line
 
@@ -30,8 +80,8 @@ def main(filename):
     f = open(filename, 'r')
     output = sys.stdout
     for line in f:
-        result = clear(line)
-        output.write(result)
+        new_line = clear(line.strip())
+        output.write(new_line + "\n")
 
     output.close()
     f.close()

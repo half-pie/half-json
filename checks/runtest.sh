f023f48ddb598efabab7db8c70848636919f1950
alingse <alingse@foxmail.com>
2019-05-29 23:59:04 +0800

    add check

--- checks/runtest.sh
+++ checks/runtest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python gen.py > random.1.json
 python broken.py random.1.json random.1.broken.json
-python clear.py random.1.broken.json random.1.broken.fix.json
+jsonfixer random.1.broken.json random.1.broken.fix.json
